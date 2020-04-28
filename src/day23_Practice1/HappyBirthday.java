762f58eda74a2a7da210667c6006987194880f25
unknown <zarina.asxodjaeva@gmail.com>
2020-04-28 16:22:56 -0700

    addHappyBirthday1.java

--- src/day23_Practice1/HappyBirthday.java
+++
@@ -1,7 +0,0 @@
-package day23_Practice1;
-
-public class HappyBirthday {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/day23_Practice1/HappyBirthday1.java
@@ -0,0 +1,7 @@
+package day23_Practice1;
+
+public class HappyBirthday1 {
+    public static void main(String[] args) {
+        System.out.println("Happy birthday B18");
+    }
+}
