947adda7bf881f991528de1e6ea35e27a5bf5e16
zarina2020 <64220846+zarina2020@users.noreply.github.com>
2020-04-28 13:36:55 -0700

    Update HappyBirthday1.java

--- src/day23_Practice1/HappyBirthday1.java
+++ src/day23_Practice1/HappyBirthday1.java
@@ -4,7 +4,7 @@ public class HappyBirthday1 {
     public static void main(String[] args) {
         System.out.println("Happy birthday B18");
     
-       System.out.println("Happy birthday again this line change from Gibhub");
+       System.out.println("Happy birthday again this line change from Github");
     
     
     }
